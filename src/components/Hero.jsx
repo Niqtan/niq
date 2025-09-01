b44622365ae4c97f4a8020d48e18f2c871b36e26
Niq S. <niqban123@gmail.com>
2025-09-01 20:46:36 +0800

    github pages configuration

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,7 +13,7 @@ export default function Hero() {
         <source src="./Hero/video.mp4" type="video/mp4" />
       </video>
       <div className="text-center text-white">
-        <h1 className="ease-in-out relative text-9xl font-bold font-monsieur mb-6 z-20">Niq Suguitan</h1>
+        <h1 className="ease-in-out relative text-5xl font-bold font-monsieur mb-6 z-20">Niq Suguitan</h1>
         <p className="animate-bounce relative text-xl font-vt323 mb-6 p-4 z-20">
           Student Engineer, Researcher, & Content Creator Highschooler
         </p>
